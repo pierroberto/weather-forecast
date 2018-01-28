5c2f63cfe3246b61ee1edde82441e39edd080e44
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-01-28 10:59:54 +0100

    Chore: create Search component

--- weather/src/app/app.module.ts
+++ weather/src/app/app.module.ts
@@ -11,9 +11,10 @@ import { WeatherService } from "./services/weather.service";
 import { ListViewComponent } from "./components/list-view/list-view.component";
 import { ItemViewComponent } from "./components/item-view/item-view.component";
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, ListViewComponent, ItemViewComponent, DashboardComponent],
+  declarations: [AppComponent, ListViewComponent, ItemViewComponent, DashboardComponent, SearchComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [WeatherService],
   bootstrap: [AppComponent]
